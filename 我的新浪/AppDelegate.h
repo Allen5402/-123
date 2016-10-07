355de7470ff806df796cfaedfe4371e72f223078
allen <540267533@qq.com>
2016-10-07 09:44:19 +0800

    Initial Commit

---
+++ 我的新浪/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  我的新浪
+//
+//  Created by allen on 16/10/7.
+//  Copyright © 2016年 allen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
